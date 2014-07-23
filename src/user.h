097ce4d645ab44a763260433265f3a570ef10d98
ThomasLee <lisihan969@gmail.com>
2014-07-23 23:02:29 +0800

    implementing...

--- src/user.h
+++ src/user.h
@@ -1,6 +1,8 @@
 #ifndef LM_USER_H_
 #define LM_USER_H_
 
+#include <memory>
+#include <iostream>
 #include <string>
 
 #include "common.h"
@@ -12,17 +14,101 @@ class User
  public:
     virtual ~User() = default;
 
-    bool VerifyPassword(const Password &password);
+    bool VerifyPassword(const Password &password) const;
 
-    virtual int access_level() const = 0;
+    virtual void Display(std::ostream &os) const = 0;
+    virtual bool Save(std::ostream &os) const;
+    virtual bool Load(std::istream &is);
 
+    // info about borrowing
+    virtual int assess_level() const { return 0; }
+
+    virtual int max_borrow() const { return 30; }
+    virtual int max_request() const { return 3; }
+
+    virtual int general_period() const { return 30; }
+    virtual int general_renewal_allowed() const { return 3; }
+
+    virtual int short_period() const { return 7; }
+    virtual int shrot_renewal_allowed() { return 3; }
+
+
+    // accessors
     const std::string & name() const { return name_; }
-    const std::vector<BookID> & books_holding() const { return books_holding_; }
+    const std::vector<BookID> & holding() const { return holding_; }
+    const std::vector<BookID> & requested() const { return requested_; }
 
  private:
     std::string name_;
     Password password_;
-    std::vector<BookID> books_holding_;
+    std::vector<BookID> holding_;
+    std::vector<BookID> requested_;
+};
+
+class UserFactory
+{
+ public:
+    virtual ~UserFactory() = default;
+
+    virtual std::unique_ptr<User> Create() = 0;
+};
+
+class Teacher : public User
+{
+ public:
+    virtual ~Teacher() = default;
+
+    virtual bool Save() const;
+    virtual bool Load();
+
+
+ private:
+
+};
+
+class TeacherFactory : public UserFactory
+{
+ public:
+    virtual ~TeacherFactory() = default;
+
+    virtual std::unique_ptr<User> Create();
+};
+
+class Student : public User
+{
+ public:
+    virtual ~Student() = default;
+
+ private:
+
+};
+
+class StudentFactory : public UserFactory
+{
+ public:
+    virtual ~StudentFactory() = default;
+
+    virtual std::unique_ptr<User> Create();
+ private:
+
+};
+
+class Administrator : public User
+{
+ public:
+    virtual ~Administrator() = default;
+
+};
+
+class AdministratorFactory : public UserFactory
+{
+ public:
+    virtual ~AdministratorFactory() = default;
+
+    virtual std::unique_ptr<User> Create();
+
+ private:
+
 };
 
 }  // namespace library_manager
