f22465b2984986387d45a98a1dd7d0231c80d20e
ThomasLee <lisihan969@gmail.com>
2014-09-18 20:50:34 +0800

    try embedded version

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-#include "command_line_interface.h"
 #include <iostream>
+#include <mysql/mysql.h>
+#include "command_line_interface.h"
 
 int main(int argc, char *argv[])
 {
+    if (mysql_library_init(0, NULL, NULL))
+    {
+        std::cerr << "could not initialize MySQL library\n";
+        return 1;
+    }
     library_manager::CommandLineInterface cli(argc, argv);
+
+    mysql_library_end();
     return cli.Run();
 }
